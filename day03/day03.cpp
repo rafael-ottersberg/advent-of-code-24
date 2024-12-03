749d3fcc5dd1dd7b141acb0141aea6e2dbf8a996
rafael.ottersberg <rafael.ottersberg@unibe.ch>
2024-12-03 11:00:13 +0100

    solve day 3 in cpp

---
+++ day03/day03.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <set>
+#include <functional>
+#include <algorithm>
+#include <filesystem>
+#include <numeric>
+#include <cstdint>
+#include <regex>
+
+#include "util.hpp"
+
+using namespace std;
+
+
+int64_t d03pt1(const filesystem::path& file_path) {
+    cout << endl << "Day 03 - Pt1: " << file_path.filename() << endl;
+    auto lines = read_lines(file_path);
+    int result = 0;
+
+    regex r("mul\\(([0-9]+),([0-9]+)\\)");
+    smatch m;
+
+    bool enabled = true;
+    for (auto line : lines) {
+        string::const_iterator searchStart(line.cbegin());
+
+        while (regex_search(searchStart, line.cend(), m, r)) {
+            result += stoi(m[1]) * stoi(m[2]);
+            searchStart = m.suffix().first;
+        }
+        
+
+    }
+    return result;
+}
+
+int64_t d03pt2(const filesystem::path& file_path) {
+    cout << endl << "Day 03 - Pt2: " << file_path.filename() << endl;
+    auto lines = read_lines(file_path);
+    int result = 0;
+
+    regex r("mul\\(([0-9]+),([0-9]+)\\)|do\\(\\)|don't\\(\\)");
+    smatch m;
+
+    bool enabled = true;
+    for (auto line : lines) {
+        string::const_iterator searchStart(line.cbegin());
+
+        while (regex_search(searchStart, line.cend(), m, r)) {
+            if (m[0] == "do()") { enabled = true; }
+            else if (m[0] == "don't()") { enabled = false; }
+            else if (enabled) { result += stoi(m[1]) * stoi(m[2]); }
+            searchStart = m.suffix().first;
+        }
+
+    }
+    return result;
+}
--- main.cpp
+++ main.cpp
@@ -2,16 +2,16 @@
 #include <iostream>
 #include <filesystem>
 #include "util.hpp"
-#include "day02/day02.hpp"
+#include "day03/day03.hpp"
 
 int main() {
     auto aoc_path = std::filesystem::path("C:\\Users\\ro22p291\\code\\advent-of-code-24\\"); // change on different machine
 
-    auto folder_path = aoc_path / "day02";
+    auto folder_path = aoc_path / "day03";
 
-    std::cout << time_function(d02pt1, folder_path / "test.txt") << std::endl;
-    std::cout << time_function(d02pt1, folder_path / "input.txt") << std::endl;
-    std::cout << time_function(d02pt2, folder_path / "test.txt") << std::endl;
-    std::cout << time_function(d02pt2, folder_path / "input.txt") << std::endl;
+    std::cout << time_function(d03pt1, folder_path / "test.txt") << std::endl;
+    std::cout << time_function(d03pt1, folder_path / "input.txt") << std::endl;
+    std::cout << time_function(d03pt2, folder_path / "test.txt") << std::endl;
+    std::cout << time_function(d03pt2, folder_path / "input.txt") << std::endl;
     return 0;
 }
