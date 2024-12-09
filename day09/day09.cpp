32501dd5808df1364d35ecbb09694881c4afd142
Rafael Ottersberg <rafael.ottersberg@gmx.ch>
2024-12-09 09:53:58 +0100

    beautify solution 09:2

--- day09/day09.cpp
+++ day09/day09.cpp
@@ -62,17 +62,22 @@ int64_t d09pt1(const filesystem::path& file_path) {
     return result;
 }
 
+struct file {
+    short id;
+    int size;
+};
+
 int64_t d09pt2(const filesystem::path& file_path) {
     auto line = read_lines(file_path)[0];
     int64_t result = 0;
 
     bool em = false;
-    int file_index = 0;
-    vector<pair<int,int>> filesystem;
+    short file_index = 0;
+    vector<file> filesystem;
 
     for (char c : line) {
         int file_size = c - '0';
-        int ind = file_index;
+        short ind = file_index;
         if (em) {
             ind = -1;
             em = false;
@@ -80,27 +85,23 @@ int64_t d09pt2(const filesystem::path& file_path) {
             em = true;
             file_index++;
         }
-        filesystem.push_back(make_pair(ind, file_size));
+        filesystem.push_back({ind, file_size});
     }
 
     for (auto rit = filesystem.rbegin(); rit != filesystem.rend(); rit++) {
-        auto move = *rit;
-        auto index = move.first;
-        auto space = move.second;
+        auto file = *rit;
 
-        if (index == -1) continue;
+        if (file.id == -1) continue;
         for (auto it = filesystem.begin(); it != filesystem.end(); it++) {
             if (it == rit.base()) break;
             auto target = *it;
-            auto t_index = target.first;
-            auto t_space  = target.second;
-            if (t_index != -1) continue;
-            if (t_space < space) continue;
+            if (target.id != -1) continue;
+            if (target.size < file.size) continue;
             *it = *rit;
-            *rit = make_pair(-1, space);
+            *rit = {-1, file.size};
             auto d = distance(it, rit.base());
-            if (t_space > space) {
-                it = filesystem.insert(it+1, make_pair(-1, t_space-space));
+            if (target.size > file.size) {
+                it = filesystem.insert(it+1, {-1, target.size-file . size});
                 rit = make_reverse_iterator(it + d);
             }
             break;
@@ -110,9 +111,9 @@ int64_t d09pt2(const filesystem::path& file_path) {
     int i = 0;
     for (auto it = filesystem.cbegin(); it != filesystem.cend(); it++) {
         auto file = *it;
-        for (int j = 0; j < file.second; j++){
-            if (file.first != -1) {
-                result += i * file.first;
+        for (int j = 0; j < file.size; j++){
+            if (file.id != -1) {
+                result += i * file.id;
             }
             i++;
         }
