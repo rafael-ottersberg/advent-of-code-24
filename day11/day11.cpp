8a4b86adf643ad27aa636240758b97f98a880d5c
Rafael Ottersberg <rafael.ottersberg@gmx.ch>
2024-12-11 15:37:49 +0100

    day11 cpp

--- day11/day11.cpp
+++ day11/day11.cpp
@@ -15,22 +15,58 @@
 
 using namespace std;
 
+int64_t solve_number(int64_t number, int blinks, unordered_map<pair<int64_t, int>, int64_t, pair_hash>& cache) {
+    if (blinks == 0) return 1;
+
+    auto p = make_pair(number, blinks);
+    if (cache.contains(p)) {
+        return cache[p];
+    }
+
+    int64_t result;
+    --blinks;
+    if (number == 0) {
+        result = solve_number(1, blinks, cache);
+        cache[p] = result;
+        return result;
+    } 
+    string n_str = to_string(number);
+    int l = n_str.size();
+    if (l % 2 == 0) {
+        int l_2 = l / 2;
+        int64_t n_left = stoll(n_str.substr(0, l_2));
+        int64_t n_right = stoll(n_str.substr(l_2, l_2));
+        result = solve_number(n_left, blinks, cache) + solve_number(n_right, blinks, cache);
+        cache[p] = result;
+        return result;
+    }
+    result = solve_number(number * 2024, blinks, cache);
+    cache[p] = result;
+    return result;
+} 
+
 int64_t d11pt1(const filesystem::path& file_path) {
     auto lines = read_lines(file_path);
-    int result = 0;
+    int64_t result = 0;
 
-    for (auto line : lines) {
-        result++;
+    auto numbers = convert_to_int32(split(lines[0], " "));
+
+    unordered_map<pair<int64_t, int>, int64_t, pair_hash> cache;
+    for (auto n : numbers) {
+        result += solve_number(n, 25, cache);
     }
     return result;
 }
 
 int64_t d11pt2(const filesystem::path& file_path) {
     auto lines = read_lines(file_path);
-    int result = 0;
+    int64_t result = 0;
 
-    for (auto line : lines) {
-        result++;
+    auto numbers = convert_to_int32(split(lines[0], " "));
+
+    unordered_map<pair<int64_t, int>, int64_t, pair_hash> cache;
+    for (auto n : numbers) {
+        result += solve_number(n, 75, cache);
     }
     return result;
 }
