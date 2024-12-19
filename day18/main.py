40bac1975f147d593349c43733cbd1ada03de025
Rafael Ottersberg <rafael.ottersberg@gmx.ch>
2024-12-19 08:54:50 +0100

    add python for day18&19

--- day18/main.py
+++ day18/main.py
@@ -6,31 +6,77 @@ parent_directory = os.path.abspath('.')
 sys.path.append(parent_directory)
 
 from helper import *
+from heapq import *
 
 def solution(input_file):
     result = 0
     lines = open(input_file, 'r').read().splitlines()
+
+    walls = set()
+
     for i, line in enumerate(lines):
-        pass
+       if i >= 1024: #1024
+           break
+       x, y = [int(n) for n in line.split(',')]
+       walls.add((x,y))
+
+    end = (70,70) #70
+
+    h = [(0, (0,0))]
+    heapify(h)
+    explored = set()
+
+    while h:
+        c, p = heappop(h)
+        if p == end:
+            return c
+        
+        for d in ADJ4:
+            sp = (p[0]+d[0], p[1]+d[1])
+            if sp not in explored and sp not in walls and 0<=sp[0]<=end[0] and 0<=sp[1]<=end[1]:
+                heappush(h, (c+1, sp))
+                explored.add(sp)
 
     return result
 
 def solution2(input_file):
     result = 0
     lines = open(input_file, 'r').read().splitlines()
-    for i, line in enumerate(lines):
-        pass
 
-    return result
+    all_walls = []
+    for i, line in enumerate(lines):
+       x, y = [int(n) for n in line.split(',')]
+       all_walls.append((x,y))
+
+    end = (70,70) #70
+
+    for i in range(2023, len(all_walls)):
+        walls = set(all_walls[:i])
+        h = [(0, (0,0))]
+        heapify(h)
+        explored = set()
+
+        while h:
+            c, p = heappop(h)
+            if p == end:
+                break
+            
+            for d in ADJ4:
+                sp = (p[0]+d[0], p[1]+d[1])
+                if sp not in explored and sp not in walls and 0<=sp[0]<=end[0] and 0<=sp[1]<=end[1]:
+                    heappush(h, (c+1, sp))
+                    explored.add(sp)
+        else:
+            return all_walls[i-1]
 
 if __name__ == '__main__':
     file_directory = pathlib.Path(__file__).parent.absolute()
-    if 1: # run part 1
-        print(benchmark(solution)(file_directory / 'test.txt'))
+    if 0: # run part 1
+        #print(benchmark(solution)(file_directory / 'test.txt'))
         print('\n*******************************\n')
         print(benchmark(solution)(file_directory / 'input.txt'))
-    if 0: # run part 2
+    if 1: # run part 2
         print('\n----------------part2----------------\n')
-        print(benchmark(solution2)(file_directory / 'test.txt'))
+        #print(benchmark(solution2)(file_directory / 'test.txt'))
         print('\n*******************************\n')
         print(benchmark(solution2)(file_directory / 'input.txt'))
--- day19/main.py
+++ day19/main.py
@@ -7,29 +7,85 @@ sys.path.append(parent_directory)
 
 from helper import *
 
+def match_pattern(p, blocks, cache):
+    if not p:
+        return True
+    
+    if p in cache:
+        return cache[p]
+    
+    for b in blocks:
+        if p.startswith(b):
+            if match_pattern(p[len(b):], blocks, cache):
+                cache[p] = True
+                return True
+            
+    cache[p] = False
+    return False
+
+def match_pattern2(p, blocks, cache):
+    if not p:
+        return 1
+    
+    if p in cache:
+        return cache[p]
+    
+    comb = 0
+    for b in blocks:
+        if p.startswith(b):
+            c = match_pattern2(p[len(b):], blocks, cache)
+            comb += c
+            
+    cache[p] = comb
+    return comb
+
 def solution(input_file):
     result = 0
+    blocks = []
+    patterns = []
     lines = open(input_file, 'r').read().splitlines()
     for i, line in enumerate(lines):
-        pass
+        if i == 0:
+            blocks = line.split(', ')
+
+        if i > 1:
+            patterns.append(line)
+
+    cache = {}
+
+    for p in patterns:
+        if match_pattern(p, blocks, cache):
+            result += 1
 
     return result
 
 def solution2(input_file):
     result = 0
+    blocks = []
+    patterns = []
     lines = open(input_file, 'r').read().splitlines()
     for i, line in enumerate(lines):
-        pass
+        if i == 0:
+            blocks = line.split(', ')
+
+        if i > 1:
+            patterns.append(line)
+
+    cache = {}
+
+    for p in patterns:
+        r= match_pattern2(p, blocks, cache)
+        result += r
 
     return result
 
 if __name__ == '__main__':
     file_directory = pathlib.Path(__file__).parent.absolute()
-    if 1: # run part 1
+    if 0: # run part 1
         print(benchmark(solution)(file_directory / 'test.txt'))
         print('\n*******************************\n')
         print(benchmark(solution)(file_directory / 'input.txt'))
-    if 0: # run part 2
+    if 1: # run part 2
         print('\n----------------part2----------------\n')
         print(benchmark(solution2)(file_directory / 'test.txt'))
         print('\n*******************************\n')
