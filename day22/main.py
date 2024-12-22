04bc6edbd8175b33bfe10754590a6d57d77aa92b
Rafael Ottersberg <rafael.ottersberg@gmx.ch>
2024-12-22 12:30:55 +0100

    day22

--- day22/main.py
+++ day22/main.py
@@ -7,19 +7,81 @@ sys.path.append(parent_directory)
 
 from helper import *
 
+def mix_prune(number, number2):
+    return (number ^ number2) % 16777216
+
+def next_secret(number):
+    number = mix_prune(number, number * 64)
+    number = mix_prune(number, number // 32)
+    number = mix_prune(number, number * 2048)
+    return number
+
+
 def solution(input_file):
     result = 0
     lines = open(input_file, 'r').read().splitlines()
+    
     for i, line in enumerate(lines):
-        pass
-
+        number = int(line)
+        for j in range(2000):
+            number = next_secret(number)
+        
+        result += number
+    
     return result
+
 
 def solution2(input_file):
     result = 0
     lines = open(input_file, 'r').read().splitlines()
+
+    seller_prices = []
+    seller_diffs = []
+
     for i, line in enumerate(lines):
-        pass
+        number = int(line)
+        last_price = 0
+        prices = []
+        diffs = ""
+        for j in range(2000):
+            number = next_secret(number)
+            price = number % 10
+            prices.append(price)
+            if j != 0:
+                d = price - last_price
+                if d >= 0:
+                    diffs += f"+{d}"
+                else:
+                    diffs += str(d)
+            last_price = price
+        
+        seller_prices.append(prices)
+        seller_diffs.append(diffs)
+
+    result = 0
+    for a in range(-9, 10):
+        for b in range(-9, 10):
+            for c in range(-9, 10):
+                for d in range(-9, 10):
+                    gain = 0
+                    s = ""
+                    for n in [a,b,c,d]:
+                        if n >= 0:
+                            s += f"+{n}"
+                        else:
+                            s += str(n)
+                
+                    j = 0
+                    for diffs, prices in zip(seller_diffs, seller_prices):
+                        i = diffs.find(s, 1)
+                                                 
+                        if i != -1:
+                            i = i // 2 + 4
+                            gain += prices[i]
+                        j+=1
+                    
+                    if gain > result:
+                        result = gain
 
     return result
 
